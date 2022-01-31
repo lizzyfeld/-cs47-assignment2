326c03340a9429e92d5df284943947241802f2d9
Elizabeth Feldman <elizabethfeldman@DN51utbc.SUNet>
2022-01-30 23:32:06 -0800

    “”

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import AppLoading from 'expo-app-loading';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View, StatusBar, Image, Container } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Themes } from './assets/Themes';
+import { TouchableWithoutFeedback } from 'react-native-web';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -16,28 +17,129 @@ export default function App() {
   /* insert your code here */
 
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontFamily: 'Sydney', // test to see if the font is loaded, feel free to remove this
-        }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Text
-        style={{
-          fontFamily: 'Sydney-Bold', // test to see if the font is loaded, feel free to remove this
-        }}>
-        ~Good luck~
-      </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.flexContainer1}>
+        <Image style={[styles.icon, {height: '70%', marginTop: '2%', marginLeft: '2%'}]} source={require("./assets/Icons/menu_light.png")} />
+        <Text style={[styles.bottomNavBarComponentText, {alignSelf: 'center', fontFamily: 'Sydney-Bold', fontSize: 32, color: Themes.light.text}]}>ensom</Text>
+        <Image style={[styles.icon, {height: '70%', marginTop: '2%', marginRight: '2%'}]} source={require("./assets/Icons/sun.png")} />
+      </View>
+      <View style={styles.flexContainer2}>
+        <ImageBackground style={styles.profilePic} source={require("./assets/Profiles/mtl.jpg")} >
+            <Text style={styles.overlayName}>MTL</Text>
+            <Text style={styles.overlayLocation}>3 miles away</Text>
+        </ImageBackground>
+        <View style={styles.myHottestViewBox}>
+            <Text style={{fontFamily: 'Sydney', fontSize: '25'}}>My hottest take</Text>
+            <View style={styles.audioIconsBox}>
+              <Image style={styles.audioIcon1} source={require("./assets/Icons/player_light.png")} />
+              <Image style={styles.audioIcon2} source={require("./assets/Icons/audio_waveform_light.png")} />
+        </View>
+        </View>
+      </View>
+      <View style={styles.flexContainer3}>
+        <View style={styles.bottomNavBarComponent}>
+          <Image style={styles.icon} source={require("./assets/Icons/discover_light.png")} />
+          <Text style={styles.bottomNavBarComponentText}>Discover</Text>
+        </View>
+        <View style={styles.bottomNavBarComponent}>
+          <Image style={styles.icon} source={require("./assets/Icons/heart_light.png")} />  
+          <Text style={styles.bottomNavBarComponentText}>Matches</Text>
+        </View>
+        <View style={styles.bottomNavBarComponent}>
+          <Image style={styles.icon} source={require("./assets/Icons/messages_light.png")} /> 
+          <Text style={styles.bottomNavBarComponentText}>DMs</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: Themes.light.bg,
     justifyContent: 'center',
   },
+  flexContainer1: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: Platform.OS === 'ios' ? 41 : 54,
+  },
+  flexContainer2: {
+    flex: 6,
+    marginTop: '10%',
+    paddingLeft: '8%',
+    paddingRight: '8%',
+    resizeMode: 'contain',
+  },
+  flexContainer3: {
+    backgroundColor: Themes.light.navigation,
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    height: '10%',
+  },
+  myHottestViewBox: {
+    backgroundColor: Themes.light.bgSecondary,
+    justifyContent: 'flex-start',
+    justifyContent: 'center',
+    height: '30%',
+    paddingLeft: '2%',
+    paddingRight: '2%',
+    borderRadius: 20,
+    marginTop: '5%',
+    paddingTop: '2%',
+  },
+  audioIconsBox: {
+    flexDirection: 'row',
+    resizeMode: 'contain',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '50%',
+  },
+  audioIcon1: {
+    flex: 1,
+    resizeMode: 'contain',
+    marginRight: '4%',
+  },
+  audioIcon2: {
+    flex: 5,
+    resizeMode: 'contain',
+  },
+  profilePic: {
+    width: '100%',
+    aspectRatio: 1,
+    resizeMode: 'contain',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  overlayName: {
+    position: "absolute",
+    color: Themes.light.bgSecondary,
+    fontFamily: 'Sydney',
+    fontSize: 32,
+    top: 8, 
+    left: 8, 
+  },
+  overlayLocation: {
+    position: "absolute",
+    fontFamily: 'Sydney',
+    fontSize: 18,
+    color: Themes.light.bgSecondary,
+    bottom: 8, 
+    left: 8, 
+  },
+  bottomNavBarComponent: {
+    alignItems:'center',
+    paddingTop: '2%',
+  },
+  icon: {
+    height: 30,
+    width: 30,
+    resizeMode: 'contain'
+  },
+  bottomNavBarComponentText: {
+    color: Themes.light.textSecondary,
+    fontFamily: 'Sydney',
+    paddingTop: '2%',
+  }
 });
